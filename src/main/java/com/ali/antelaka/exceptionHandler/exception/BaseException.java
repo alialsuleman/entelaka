28c07691032b75596f9736a929419da94fc6349e
alialsuleman <alsulemanali08@gmail.com>
2026-05-09 10:35:54 +0200

    refactor: add custom exception hierarchy

---
+++ src/main/java/com/ali/antelaka/exceptionHandler/exception/BaseException.java
@@ -0,0 +1,34 @@
+package com.ali.antelaka.exceptionHandler.exception;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@Builder
+public abstract class BaseException extends RuntimeException {
+    private final HttpStatus status;
+    private final String errorCode;
+    private final Object additionalData;
+
+    public BaseException(String message, HttpStatus status, String errorCode) {
+        this(message, status, errorCode, null);
+    }
+
+    public BaseException(String message, HttpStatus status, String errorCode, Object additionalData) {
+        super(message);
+        this.status = status;
+        this.errorCode = errorCode;
+        this.additionalData = additionalData;
+    }
+
+    public BaseException(String message, HttpStatus status, String errorCode, Throwable cause) {
+        super(message, cause);
+        this.status = status;
+        this.errorCode = errorCode;
+        this.additionalData = null;
+    }
+}
---
+++ src/main/java/com/ali/antelaka/exceptionHandler/exception/ConflictException.java
@@ -0,0 +1,14 @@
+package com.ali.antelaka.exceptionHandler.exception;
+
+
+import org.springframework.http.HttpStatus;
+
+public class ConflictException extends BaseException {
+    public ConflictException(String message) {
+        super(message, HttpStatus.CONFLICT, "CONFLICT");
+    }
+
+    public ConflictException(String message, Object additionalData) {
+        super(message, HttpStatus.CONFLICT, "CONFLICT", additionalData);
+    }
+}
